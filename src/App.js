ef6beb3aae799be4348178407ea317fc816ba3e9
Sina NAMAKI <sina_namaki@yahoo.com>
2021-12-03 17:08:00 +0100

    update the menu items to navigate with routers to different shops

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage';
 
+const HatsPage = ()=>{
+  return(
+    <div>
+      
+    <h1>Hats page </h1>
+  </div>
+  )
+  
+}
+// 
+// const TopicList = (props) =>{
+//   return(
+//     <div>
+//       {/* /Dynamic Routing */}
+//       <h1>TOPIC LIST PAGE</h1>
+//       <Link to= {`${props.match.url}/13`}>To TOPIC 13</Link>
+//       <Link to= {`${props.match.url}/18`}>To TOPIC 18</Link>
+//       <Link to= {`${props.match.url}/21`}>To TOPIC 21</Link>
+//     </div>
+//   )
+// }
+// 
+// const TopicDetail = props =>{
+//   console.log(props);
+//   return(
+//     <div>
+//       <h1>Topic Detail page : {props.match.params.topicId}</h1>
+//     </div>
+//   )
+// }
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route  exact path='/' component={HomePage}/>
+        <Route  path='/shop/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
